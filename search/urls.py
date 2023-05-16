71aa747aea9e0e27a675eeb59044b3a4ac769867
Omar Faruk <Omarfaruk885@gmail.com>
2023-05-17 05:02:45 +0600

    Update Paginator with Css

--- search/urls.py
+++ search/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('word', views.word, name='word'),
     path('insert/', views.insert_data, name='insert_data'),
     path('display_data/', views.display_data, name='display_data'),
+    path('my_view', views.my_view, name='my_view'),
 ]
--- search/views.py
+++ search/views.py
@@ -48,8 +48,46 @@ def display_data(request):
     else:
         words = Dic.objects.all()
     
-    paginator = Paginator(words, 10)  # Show 5 words per page
+    paginator = Paginator(words, 5) # Show 5 data per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
     return render(request, 'display_data.html', {'page_obj': page_obj, 'query': query})
+
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+def my_view(request):
+    data = [
+        {'name': 'Alice', 'age': 22},
+        {'name': 'Bob', 'age': 35},
+        {'name': 'Charlie', 'age': 18},
+        {'name': 'David', 'age': 27},
+        {'name': 'Emily', 'age': 29},
+        {'name': 'Frank', 'age': 41},
+        {'name': 'Gina', 'age': 25},
+        {'name': 'Harry', 'age': 32},
+        {'name': 'Ivy', 'age': 19},
+        {'name': 'Jack', 'age': 38},
+        {'name': 'Kate', 'age': 23},
+        {'name': 'Liam', 'age': 28},
+        {'name': 'Mia', 'age': 31},
+        {'name': 'Nate', 'age': 24},
+        {'name': 'Olivia', 'age': 26},
+        {'name': 'Paul', 'age': 37},
+        {'name': 'Queenie', 'age': 21},
+        {'name': 'Ryan', 'age': 34},
+        {'name': 'Sarah', 'age': 30},
+        {'name': 'Tom', 'age': 33},
+        {'name': 'Ursula', 'age': 20},
+        {'name': 'Victor', 'age': 36},
+        {'name': 'Wendy', 'age': 39},
+        {'name': 'Xavier', 'age': 40},
+        {'name': 'Yvonne', 'age': 42},
+        {'name': 'Zoe', 'age': 43},
+    ]
+
+    paginator = Paginator(data, 5) # Show 5 data per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'my_view.html', {'page_obj': page_obj})
